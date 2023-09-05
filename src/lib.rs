34f4074e5b58d8e76ff51deeda942902bd18546b
Pranav Teegavarapu <pranavnt@outlook.com>
2023-09-05 04:56:44 -0700

    generally works???

--- src/lib.rs
+++ src/lib.rs
@@ -1,16 +1,18 @@
 extern crate piston_window;
+extern crate image;
 
-use piston_window::*;
+use image::{ImageBuffer, Rgb, RgbImage};
+use std::path::Path;
+use std::process::Command;
 
 pub struct Board {
-    size: usize,
-    grid: Vec<Cell>,
-    frames: Vec<Vec<Cell>>,
-    f_index: usize,
-    rule: Box<dyn Rule>,
+    pub size: usize,
+    pub grid: Vec<Cell>,
+    pub frames: Vec<Vec<Cell>>,
+    pub rule: Box<dyn Rule>,
 }
 
-#[derive(Clone, Copy, PartialEq)]
+#[derive(Debug, Clone, Copy, PartialEq)]
 pub enum Cell {
     Alive,
     Dead,
@@ -24,46 +26,50 @@ impl Board {
     pub fn new(size: usize, rule: Box<dyn Rule>) -> Board{
         Board {
             size,
-            grid: vec![Cell::Dead; size as usize],
+            grid: vec![Cell::Dead; size * size],
             frames: Vec::<Vec<Cell>>::new(),
-            f_index: 0,
             rule,
         }
     }
 
     pub fn set(&mut self, x: usize, y: usize, value: Cell) {
-        self.grid[x * self.size + y] = value
+        self.grid[x * self.size + y] = value;
+        self.frames.push(self.grid.clone());
     }
 
     pub fn tick(&mut self) {
         self.grid = self.rule.apply(&self.grid);
         self.frames.push(self.grid.clone());
     }
-
-    pub fn render_video(&self) {
-        let mut window: PistonWindow = WindowSettings::new("Game of Life", [self.size as u32, self.size as u32])
-            .exit_on_esc(true)
-            .build()
-            .unwrap();
-        let mut frame_index = 0;
-        while let Some(event) = window.next() {
-            if let Some(_) = event.render_args() {
-                let frame = &self.frames[frame_index];
-                window.draw_2d(&event, |context: Context, graphics, _| {
-                    clear([0.0, 0.0, 0.0, 1.0], graphics);
-                    for (pos, cell) in self.grid.iter().enumerate() {
-                        let color: [f32; 4] = match cell {
-                            Cell::Alive => [1.0, 1.0, 1.0, 1.0],
-                            Cell::Dead => [0.0, 0.0, 0.0, 1.0],
-                        };
-                        let x: usize = pos % self.size;
-                        let y: usize = pos / self.size;
-                        let rect: [f64; 4] = rectangle::square(x as f64, y as f64, 1.0);
-                        rectangle(color, rect, context.transform, graphics);
-                    }
-                });
-                frame_index = (frame_index + 1) % self.frames.len();
+   
+    pub fn render(&self, dir_name: &str) {
+        let mut i = 0;
+        for frame in &self.frames {
+            let mut img: RgbImage = ImageBuffer::new(self.size as u32, self.size as u32);
+            for (x, y, pixel) in img.enumerate_pixels_mut() {
+                let cell = frame[(x * self.size as u32 + y) as usize];
+                let color = match cell {
+                    Cell::Alive => Rgb([255, 255, 255]),
+                    Cell::Dead => Rgb([0, 0, 0]),
+                };
+                *pixel = color;
             }
+            let path = Path::new(dir_name + "/" + &i.to_string());
         }
+        // let _ = Command::new("ffmpeg")
+        //     .arg("-y")
+        //     .arg("-i")
+        //     .arg(format!("{}.png", path.to_str().unwrap()))
+        //     .arg("-c:v")
+        //     .arg("libx264")
+        //     .arg("-pix_fmt")
+        //     .arg("yuv420p")
+        //     .arg("-crf")
+        //     .arg("23")
+        //     .arg("-preset")
+        //     .arg("medium")
+        //     .arg(format!("{}.mp4", path.to_str().unwrap()))
+        //     .output()
+        //     .unwrap();
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -3,5 +3,17 @@ mod lib;
 
 
 fn main() {
-    let conway_board = lib::Board::new(100, Box::new(conway::ConwayRule {}));
+    let mut conway_board = lib::Board::new(100, Box::new(conway::ConwayRule {}));
+
+    // initialize the board
+    conway_board.set(50, 50, lib::Cell::Alive);
+    conway_board.set(51, 50, lib::Cell::Alive);
+
+    // run the simulation
+    for _ in 0..1000 {
+        conway_board.tick();
+    }
+
+    // render the video
+    conway_board.render("./videos/conway.mp4");
 }
